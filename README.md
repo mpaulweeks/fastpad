647b60780d0b156c5352fd1d73ae8f3d54d0a4c1
mpaulweeks <mpaulweeks@gmail.com>
2018-11-23 02:27:58 -0500

    simply and hide nav, get ordered tabs working with some magic

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ Fast native-app notepad with encrypted cloud storage
 ## resources
 
 - [useful simple example of redux](https://codesandbox.io/s/github/reactjs/redux/tree/master/examples/todos)
+- https://medium.com/async-la/a-stately-guide-to-react-navigation-with-redux-1f90c872f96e
 
 ## todo
 
--- pkg/native/navigation/AppNavigator.js
+++ pkg/native/navigation/AppNavigator.js
@@ -1,16 +1,59 @@
 import React from 'react';
-import { createSwitchNavigator } from 'react-navigation';
-
-import MainTabNavigator from './MainTabNavigator';
-
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import rootReducer from '../redux/reducers';
+import {
+  createSwitchNavigator,
+  createMaterialTopTabNavigator,
+  createStackNavigator,
+} from 'react-navigation';
+
+import HomeScreen from '../screens/HomeScreen';
+import CreateNoteScreen from '../screens/CreateNoteScreen';
+import ListNotesScreen from '../screens/ListNotesScreen';
+import SettingsScreen from '../screens/SettingsScreen';
+
+const screens = [
+  {
+    name: 'Settings',
+    screen: SettingsScreen,
+  },
+  {
+    name: 'Home',
+    screen: HomeScreen,
+  },
+  {
+    name: 'ListNotes',
+    screen: ListNotesScreen,
+  },
+  {
+    name: 'CreateNote',
+    screen: CreateNoteScreen,
+  },
+];
+
+const stacks = screens.reduce((obj, s) => {
+  obj[s.name] = createStackNavigator({
+    [s.name]: s.screen,
+  });
+  return obj;
+}, {});
+
+const MainNavigator = createMaterialTopTabNavigator(stacks, {
+  order: ['Settings', 'Home', 'ListNotes', 'CreateNote'],
+  animationEnabled: true,
+  initialRouteName: 'CreateNote',
+  navigationOptions: {
+    tabBarVisible: false,
+  },
+});
 
 const AppNavigator = createSwitchNavigator({
   // You could add another route here for authentication.
   // Read more at https://reactnavigation.org/docs/en/auth-flow.html
-  Main: MainTabNavigator,
+  Main: MainNavigator,
+}, {
+  initialRouteName: 'Main',
 });
 
 const store = createStore(rootReducer);
