4b90f9794784dcb097d7824754d6da48e0a16817
mpaulweeks <mpaulweeks@gmail.com>
2018-11-24 01:50:30 -0500

    pattern to de-dupe DataStore code

--- pkg/native/utils/DataStore.js
+++ pkg/native/utils/DataStore.js
@@ -31,81 +31,67 @@ class _Api {
     }
     return response.json();
   }
-
-  fetchNotes(){
+  generateDispatcher = (begin, fetch, success, failure) => {
     return async (dispatch) => {
-      let notes = null;
       try {
-        await dispatch(fetchNotesBegin());
-        const response = await fetch(`${baseUrl}/notes`, {
-          method: 'GET',
-          headers: {
-            'apikey': this.apikey,
-          },
-        });
+        await dispatch(begin());
+        const response = await fetch();
         const result = await this.jsonOrErrors(response);
-        await dispatch(fetchNotesSuccess(result.notes));
+        await dispatch(success(result));
       } catch (error) {
-        await dispatch(fetchNotesFailure(error));
-      }
-    }
-  }
-  deleteNote(id){
-    return async (dispatch) => {
-      try {
-        await dispatch(deleteNoteBegin(id));
-        const response = await fetch(`${baseUrl}/notes/${id}`, {
-          method: 'DELETE',
-          headers: {
-            'apikey': this.apikey,
-          },
-        });
-        await this.jsonOrErrors(response);
-        await dispatch(deleteNoteSuccess());
-      } catch (error) {
-        await dispatch(deleteNoteFailure(error));
+        await dispatch(failure(error));
       }
     }
   }
 
-  createNote(text){
-    return async (dispatch) => {
-      try {
-        await dispatch(createNoteBegin(text));
-        const response = await fetch(`${baseUrl}/notes`, {
-          method: 'POST',
-          headers: {
-            'apikey': this.apikey,
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({text: text}),
-        });
-        const result = await this.jsonOrErrors(response);
-        await dispatch(createNoteSuccess(result.note));
-      } catch (error) {
-        await dispatch(createNoteFailure(error));
-      }
-    }
-  }
-  updateNote(id, text){
-    return async (dispatch) => {
-      try {
-        await dispatch(updateNoteBegin(id, text));
-        const response = await fetch(`${baseUrl}/notes/${id}`, {
-          method: 'PATCH',
-          headers: {
-            'apikey': this.apikey,
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({text: text}),
-        });
-        const result = await this.jsonOrErrors(response);
-        await dispatch(updateNoteSuccess(result.note));
-      } catch (error) {
-        await dispatch(updateNoteFailure(error));
-      }
-    }
-  }
+  fetchNotes = () => this.generateDispatcher(
+    () => fetchNotesBegin(),
+    () => fetch(`${baseUrl}/notes`, {
+      method: 'GET',
+      headers: {
+        'apikey': this.apikey,
+      },
+    }),
+    result => fetchNotesSuccess(result.notes),
+    err => fetchNotesFailure(err),
+  );
+  deleteNote = id => this.generateDispatcher(
+    () => deleteNoteBegin(),
+    () => fetch(`${baseUrl}/notes/${id}`, {
+      method: 'DELETE',
+      headers: {
+        'apikey': this.apikey,
+      },
+    }),
+    result => deleteNoteSuccess(),
+    err => deleteNoteFailure(err),
+  );
+  createNote = text => this.generateDispatcher(
+    () => createNoteBegin(text),
+    () => fetch(`${baseUrl}/notes`, {
+      method: 'POST',
+      headers: {
+        'apikey': this.apikey,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({text: text}),
+    }),
+    result => createNoteSuccess(result.note),
+    err => createNoteFailure(err),
+  );
+  updateNote = (id, text) => this.generateDispatcher(
+    () => updateNoteBegin(id, text),
+    () => fetch(`${baseUrl}/notes/${id}`, {
+      method: 'PATCH',
+      headers: {
+        'apikey': this.apikey,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({text: text}),
+    }),
+    result => updateNoteSuccess(result.note),
+    err => updateNoteFailure(err),
+  );
 }
 
 const Api = new _Api();
